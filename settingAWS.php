8bb0c74728557ff2d56630dc7c1067ac2eaa1145
FatherAntelope <43047846+FatherAntilopi@users.noreply.github.com>
2020-08-27 19:33:49 +0500

    ubdate

--- settingAWS.php
+++ settingAWS.php
@@ -14,8 +14,8 @@
  * @var integer $maxpages maximum number of search pages for Springer
  */
 $attributes = "ID, Title, Abstract, Authors, Publisher, PublicationName, PublicationDate, Link, PDFLink";
-$access_key = 'AKIA5CG3AZL6UP7DIT64';
-$secret_key = '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';
+$access_key = 'AKIA5CG3AZL6UP7DIT64'; //СТАРЫЙ//AKIA5CG3AZL6UP7DIT64 | //НОВЫЙ//AKIAWD6E4DHGLUOJ3RM2
+$secret_key = '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';//СТАРЫЙ//'/lUQplZ/C1Ptamo5yXsvB4E9vzU/S4cUDiq5wA63'; //НОВЫЙ//fKBW4IqhDPkGBGQHxhUqcl+NBepAPA4K2eQ4S+ee
 $region = 'us-east-2';
 $tableNameEnglish = 'englishDataTable';
 $tableNameRussian = 'russianDataTable';
